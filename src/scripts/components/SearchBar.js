210407d9e680c53e4abe363b6599169515cc0dd9
James Shin <jhscode@gmail.com>
2018-09-07 08:39:48 -0400

    refactored event

--- src/scripts/components/SearchBar.js
+++ src/scripts/components/SearchBar.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const apiKey = process.env.REACT_APP_API_KEY;
+const ROOT_URL = `http://lcboapi.com/products?access_key=${apiKey}`
+
 class SearchBar extends Component {
+
+  searchName = (e) => {
+    e.preventDefault();
+    const search = e.target.elements.search.value;
+    const url = `${ROOT_URL}&q=${search}`
+    axios.get(url)
+    .then((res) => {
+      console.log(res);
+    })
+  }
 
   render() {
     return (

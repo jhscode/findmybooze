45dc1304d1120c1831be40411e2b84403380a28d
James Shin <jhscode@gmail.com>
2018-09-08 17:05:33 -0400

    axios call and store data

--- src/scripts/components/GoogleMaps.js
+++ src/scripts/components/GoogleMaps.js
@@ -1,1 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class GoogleMaps extends Component {
+  render() {
+    return (
+      <div>
+        Google Maps
+      </div>
+    )
+  }
+}
+
+export default GoogleMaps;
--- src/scripts/components/SearchBar.js
+++ src/scripts/components/SearchBar.js
@@ -5,34 +5,43 @@ const apiKey = process.env.REACT_APP_API_KEY;
 const ROOT_URL = `http://lcboapi.com/products?access_key=${apiKey}`
 
 class SearchBar extends Component {
+  state = {
+    type:'',
+    data: []
+  }
 
   searchName = (e) => {
     e.preventDefault();
-    const search = e.target.elements.search.value;
-    const url = `${ROOT_URL}&q=${search}`
+    const price = e.target.elements.price.value;
+    const type = e.target.elements.type.value;
+    const url = `${ROOT_URL}&q=${type}&order=price_in_cents.desc`;
     axios.get(url)
     .then((res) => {
       console.log(res);
+      console.log(res.data.result);
+      this.setState = ({
+        data: res.data.result
+      })
     })
   }
 
   render() {
     return (
       <div className="searchBar-wrapper">
-        <form onSubmit={this.props.searchName}>
+        <form onSubmit={this.searchName}>
           <input
             type="text"
-            name="search"
+            name="type"
             placeholder="What are you thirsty for?"
           ></input>
-          <button>Submit</button>
-        </form>
-        <select>
-          How much do you want to pay?
+        <select name="price">          
+          <option value="selectPrice">Select Price</option>
           <option value="priceOne">$</option>
           <option value="priceTwo">$$</option>
           <option value="priceThree">$$$</option>
         </select>
+          <button>Submit</button>
+        </form>
       </div>
     );
   } 
